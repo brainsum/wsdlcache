2886e6cc8a1ea5203d9739b25105165880c588dc
mhavelant <mhavelant@brainsum.com>
2016-08-02 14:32:10 +0200

    Mail config.

--- config/mail.php
+++ config/mail.php
@@ -3,9 +3,9 @@
 // @see sample at https://github.com/laravel/laravel/blob/master/config/mail.php
 
 return array(
-  "driver" => env("MAIL_DRIVER", "smtp"),
-  "host" => env("MAIL_HOST", "mailtrap.io"),
-  "port" => env("MAIL_PORT", 2525),
+  "driver" => env("MAIL_DRIVER", null),
+  "host" => env("MAIL_HOST", null),
+  "port" => env("MAIL_PORT", null),
   "from" => array(
     "address" => env("MAIL_FROM_ADDRESS", "from@example.com"),
     "name" => env("MAIL_FROM_NAME", "Example")
@@ -16,6 +16,6 @@ return array(
   ),
   "username" => env("MAIL_USERNAME", null),
   "password" => env("MAIL_PASSWORD", null),
-  "sendmail" => "/usr/sbin/sendmail -bs",
+  "sendmail" => env("MAIL_SENDMAIL", null),
   "pretend" => false
 );
